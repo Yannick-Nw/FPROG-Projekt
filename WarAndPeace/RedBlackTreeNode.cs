340ab6c2da41bc2725cfb1632d44e1c2ed04f284
Lilli Gruber <lilligruber02@gmail.com>
2024-11-11 13:28:03 +0100

    Added war and peace and Node class

---
+++ WarAndPeace/RedBlackTreeNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarAndPeace
+{
+    public enum Color { RED, BLACK };
+
+    public class RedBlackTreeNode<TValue>
+    {
+        public TValue Value { get; set; }
+        Color Color { get; set; }
+        RedBlackTreeNode<TValue> Left;
+        RedBlackTreeNode<TValue> Right;
+        RedBlackTreeNode<TValue> Parent;
+
+        public static RedBlackTreeNode<TValue> CreateNode(TValue value, Color color)
+        {
+            return new RedBlackTreeNode<TValue>(value, color);
+        }
+
+        internal RedBlackTreeNode(TValue value, Color color)
+        {
+            Value = value;
+            Color = color;
+        }
+
+    }
+}
