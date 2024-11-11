dcc12ecc3b9d74701edbec7f6c356da3fb48c112
Lilli Gruber <lilligruber02@gmail.com>
2024-11-11 13:45:54 +0100

    Adapted node and made readfile function

--- WarAndPeace/Program.cs
+++ WarAndPeace/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace WarAndPeace
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace WarAndPeace
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = ReadWholeFile("war_and_peace.txt"); //Read text of file
         }
+
+        public static string ReadWholeFile(string path) => File.ReadAllText(path);
     }
 }
--- WarAndPeace/RedBlackTreeNode.cs
+++ WarAndPeace/RedBlackTreeNode.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WarAndPeace
 {
@@ -10,22 +6,41 @@ namespace WarAndPeace
 
     public class RedBlackTreeNode<TValue>
     {
-        public TValue Value { get; set; }
-        Color Color { get; set; }
-        RedBlackTreeNode<TValue> Left;
-        RedBlackTreeNode<TValue> Right;
-        RedBlackTreeNode<TValue> Parent;
+        public TValue Value { get; }
+        public Color Color { get; }
+        public RedBlackTreeNode<TValue> Left { get; }
+        public RedBlackTreeNode<TValue> Right { get; }
+        public RedBlackTreeNode<TValue> Parent { get; }
 
-        public static RedBlackTreeNode<TValue> CreateNode(TValue value, Color color)
-        {
-            return new RedBlackTreeNode<TValue>(value, color);
-        }
-
-        internal RedBlackTreeNode(TValue value, Color color)
+        private RedBlackTreeNode(TValue value, Color color,
+                                 RedBlackTreeNode<TValue> left = null,
+                                 RedBlackTreeNode<TValue> right = null,
+                                 RedBlackTreeNode<TValue> parent = null)
         {
             Value = value;
             Color = color;
+            Left = left;
+            Right = right;
+            Parent = parent;
         }
 
+        public static RedBlackTreeNode<TValue> CreateRootNode(TValue value, Color color) =>
+            new RedBlackTreeNode<TValue>(value, color);
+
+        //Add new node with left child
+        public RedBlackTreeNode<TValue> AddLeftChild(TValue value, Color color) =>
+            new RedBlackTreeNode<TValue>(value, color, parent: this);
+
+        //Add new node with right child
+        public RedBlackTreeNode<TValue> AddRightChild(TValue value, Color color) =>
+            new RedBlackTreeNode<TValue>(value, color, parent: this);
+
+        //Change existing left node
+        public RedBlackTreeNode<TValue> WithLeft(RedBlackTreeNode<TValue> newLeft) =>
+            new RedBlackTreeNode<TValue>(Value, Color, newLeft, Right, Parent);
+
+        //Change existing right node
+        public RedBlackTreeNode<TValue> WithRight(RedBlackTreeNode<TValue> newRight) =>
+            new RedBlackTreeNode<TValue>(Value, Color, Left, newRight, Parent);
     }
 }
